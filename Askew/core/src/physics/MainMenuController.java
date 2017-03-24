8dbf808165ca60239fea16185d44d2cddc395404
arklian <emeraldarrow@gmail.com>
2017-03-24 05:58:36 -0400

    started main menu controller

--- Askew/core/src/physics/MainMenuController.java
+++ Askew/core/src/physics/MainMenuController.java
@@ -1,8 +1,10 @@
 package physics;
 
 import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.*;
+import lombok.Setter;
 import physics.InputController;
 import physics.WorldController;
 import physics.obstacle.ComplexObstacle;
@@ -16,8 +18,48 @@ import lombok.Getter;
 
 
 public class MainMenuController extends WorldController {
+    @Getter @Setter
+    private int selected = 0;
+    @Getter @Setter
+    private int minLevel = 0;
+    @Getter @Setter
+    private int maxLevel = 10;
+
 
     public MainMenuController() {
+    }
+
+    @Override
+    public boolean preUpdate(float dt) {
+        if (!super.preUpdate(dt)) {
+            return false;
+        }
+
+        InputController input = InputController.getInstance();
+
+        if (input.didLeftButtonPress()) {
+            System.out.println("GM");
+            listener.exitScreen(this, EXIT_MM_GM);
+            return false;
+        } else if (input.didTopButtonPress()) {
+            System.out.println("MM");
+            listener.exitScreen(this, EXIT_MM_LE);
+            return false;
+        }
+
+
+
+        return true;
+
+    }
+
+    @Override
+    public void draw(float delta) {
+        canvas.clear();
+        displayFont.setColor(Color.GREEN);
+        canvas.begin(); // DO NOT SCALE
+        canvas.drawTextCentered("Level: " + selected, displayFont, 0.0f);
+        canvas.end();
     }
 
     @Override
@@ -27,6 +69,13 @@ public class MainMenuController extends WorldController {
 
     @Override
     public void update(float dt) {
+        InputController input = InputController.getInstance();
+        if(input.didLeftDPadPress() && selected < maxLevel) {
+            selected++;
+        }
+        else if(input.didRightDPadPress() && selected > minLevel){
+            selected--;
+        }
 
     }
 }
