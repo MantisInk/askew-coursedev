faa574b13c3344dcfa4919d921d27e005be2f661
arklian <emeraldarrow@gmail.com>
2017-03-23 23:40:58 -0400

    renamed project to askew

---
+++ Askew/core/src/physics/MainMenuController.java
@@ -0,0 +1,32 @@
+package physics;
+
+import com.badlogic.gdx.assets.AssetManager;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.*;
+import physics.InputController;
+import physics.WorldController;
+import physics.obstacle.ComplexObstacle;
+import physics.obstacle.Obstacle;
+import physics.platform.StiffBranch;
+import physics.platform.Vine;
+import physics.platform.sloth.SlothModel;
+import util.PooledList;
+import lombok.Getter;
+
+
+
+public class MainMenuController extends WorldController {
+
+    public MainMenuController() {
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void update(float dt) {
+
+    }
+}
---
+++ Askew/core/src/physics/leveleditor/JSONLoaderSaver.java
@@ -0,0 +1,89 @@
+package physics.leveleditor;
+
+
+import com.badlogic.gdx.math.Vector2;
+import com.google.gson.*;
+import physics.obstacle.Obstacle;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Optional;
+
+public class JSONLoaderSaver {
+
+    private Gson gson;
+
+    @Setter @Getter
+    private Vector2 scale;
+
+    public JSONLoaderSaver() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setPrettyPrinting();
+        gsonBuilder.registerTypeAdapter(Obstacle.class, new EntityWrapper(this));
+        gson = gsonBuilder.create();
+    }
+
+    public LevelModel loadLevel(String levelName) throws FileNotFoundException {
+        FileReader fr = new FileReader("./levels/" + levelName + ".json");
+        LevelModel loaded = gson.fromJson(fr, LevelModel.class);
+        return loaded;
+    }
+
+    public boolean saveLevel(LevelModel toSave, String levelName) {
+        try {
+            FileWriter fw = new FileWriter("./levels/" + levelName + ".json");
+            gson.toJson(toSave, fw);
+            fw.flush();
+            fw.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+
+    public String gsonToJson(Obstacle o) {
+        return gson.toJson(o, Obstacle.class);
+    }
+
+    public Obstacle obstacleFromJson(String s) {
+        return gson.fromJson(s, Obstacle.class);
+    }
+
+    /**
+     * Loads an arbitrary file into a generic JSON object.
+     * @param assetPath the path in assets referencing the file
+     * @return the JsonObject optional
+     */
+    public static Optional<JsonObject> loadArbitrary(String assetPath) {
+        FileReader fr = null;
+        try {
+            fr = new FileReader(assetPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+            return Optional.empty();
+        }
+        JsonParser jp = new JsonParser();
+        JsonElement je = jp.parse(fr);
+        return Optional.of(je.getAsJsonObject());
+    }
+
+    public static void saveArbitrary(String s, String text) {
+        try {
+            FileWriter fw = new FileWriter(s);
+            fw.write(text);
+            fw.close();
+        } catch (IOException e) {
+            // Not much we can do, unlikely to happen anyway.
+            e.printStackTrace();
+        }
+    }
+
+    public String prettyJson(JsonObject notPrettyJson) {
+        return gson.toJson(notPrettyJson);
+    }
+}
