1368d0e92c58cb0df28af530266bedc01e04dcac
echyam <echyam@gmail.com>
2017-04-20 22:44:19 -0400

    added pause

--- Askew/core/src/askew/playermode/gamemode/GameModeController.java
+++ Askew/core/src/askew/playermode/gamemode/GameModeController.java
@@ -56,6 +56,9 @@ public class GameModeController extends WorldController {
 	/** Track asset loading from all instances and subclasses */
 	@Getter
 	private static boolean playerIsReady = false;
+	private boolean paused = false;
+	private boolean prevPaused = false;
+	private Vector2[] pause_locs = {new Vector2(11f,4.8f), new Vector2(9f,3.9f), new Vector2(11f,3f)};
 
 	@Setter
 	private String loadLevel, DEFAULT_LEVEL;
@@ -69,7 +72,13 @@ public class GameModeController extends WorldController {
 	private JSONLoaderSaver jsonLoaderSaver;
 	private float initFlowX;
 	private float initFlowY;
+	private int PAUSE_RESUME = 0;
+	private int PAUSE_RESTART = 1;
+	private int PAUSE_MAINMENU = 2;
+	private int pause_mode = PAUSE_RESUME;
 	private Texture background;
+	private Texture pauseTexture;
+	private Texture fern;
 
 	/**
 	 * Preloads the assets for this controller.
@@ -109,6 +118,8 @@ public class GameModeController extends WorldController {
 		//SoundController sounds = SoundController.getInstance();
 		SoundController.getInstance().allocate(manager, "sound/music/askew.wav");
 		background = manager.get("texture/background/background1.png", Texture.class);
+		pauseTexture = manager.get("texture/background/pause.png", Texture.class);
+		fern = manager.get("texture/background/fern.png");
 
 		super.loadContent(manager);
 		platformAssetState = AssetState.COMPLETE;
@@ -165,6 +176,17 @@ public class GameModeController extends WorldController {
 			loadLevel = "level"+lvl;
 	}
 
+	public void pause(){
+		if (!paused) {
+			paused = true;
+			pause_mode = PAUSE_RESUME;
+		}
+		else {
+			paused = false;
+		}
+		playerIsReady = false;
+	}
+
 	/**
 	 * Resets the status of the game so that we can play again.
 	 *
@@ -173,6 +195,7 @@ public class GameModeController extends WorldController {
 	public void reset() {
 
 		playerIsReady = false;
+		paused = false;
 		collisions.clearGrab();
 		Vector2 gravity = new Vector2(world.getGravity() );
 
@@ -255,6 +278,7 @@ public class GameModeController extends WorldController {
 		if (!super.preUpdate(dt)) {
 			return false;
 		}
+
 		InputController input = InputController.getInstance();
 
 		if (input.didLeftButtonPress() || input.isLKeyPressed()) {
@@ -265,6 +289,28 @@ public class GameModeController extends WorldController {
 			System.out.println("MM");
 			listener.exitScreen(this, EXIT_GM_MM);
 			return false;
+		}
+
+		if (paused) {
+			System.out.println("paused in update");
+			//InputController input = InputController.getInstance();
+			if (input.didBottomButtonPress() && pause_mode == PAUSE_RESUME) {
+				paused = false;
+				playerIsReady = false;
+			} else if (input.didBottomButtonPress() && pause_mode == PAUSE_RESTART) {
+				reset();
+			} else if (input.didBottomButtonPress() && pause_mode == PAUSE_MAINMENU) {
+				System.out.println("MM");
+				listener.exitScreen(this, EXIT_GM_MM);
+			}
+
+			if (input.didTopDPadPress() && pause_mode > 0) {
+				pause_mode--;
+			}
+			if (input.didBottomDPadPress() && pause_mode < 2) {
+				pause_mode++;
+			}
+			System.out.println("pause mode is " + pause_mode);
 		}
 
 		//Checks to see if player has selected the button on the starting screen
@@ -288,6 +334,9 @@ public class GameModeController extends WorldController {
 	 */
 	public boolean checkReady(){
 		InputController theController = InputController.getInstance();
+
+		if (paused)
+			return false;
 
 		//If the player pressed "RB"
 		if(theController.getRightGrab()){
@@ -318,65 +367,72 @@ public class GameModeController extends WorldController {
 	 * @param dt Number of seconds since last animation frame
 	 */
 	public void update(float dt) {
-		// Process actions in object model
-		sloth.setLeftHori(InputController.getInstance().getLeftHorizontal());
-		sloth.setLeftVert(InputController.getInstance().getLeftVertical());
-		sloth.setRightHori(InputController.getInstance().getRightHorizontal());
-		sloth.setRightVert(InputController.getInstance().getRightVertical());
-		sloth.setLeftGrab(InputController.getInstance().getLeftGrab());
-		sloth.setRightGrab(InputController.getInstance().getRightGrab());
-		sloth.setLeftStickPressed(InputController.getInstance().getLeftStickPressed());
-		sloth.setRightStickPressed(InputController.getInstance().getRightStickPressed());
-		currentTime += dt;
+		System.out.println("paused "+paused);
+		if (paused) {
 
-		//#TODO Collision states check
-		setFailure(collisions.isFlowKill());
-
-		Body leftCollisionBody = collisions.getLeftBody();
-		Body rightCollisionBody = collisions.getRightBody();
-
-		if (collisions.isFlowWin()) {
-			System.out.println("VICTORY");
-			setComplete(true);
-		}
-
-		// Physics tiem
-		// Gribby grab
-		if (sloth.isLeftGrab()) {
-			sloth.grab(world,leftCollisionBody, true);
 		} else {
-			sloth.releaseLeft(world);
-		}
+			System.out.println("not paused in update");
+			// Process actions in object model
+			sloth.setLeftHori(InputController.getInstance().getLeftHorizontal());
+			sloth.setLeftVert(InputController.getInstance().getLeftVertical());
+			sloth.setRightHori(InputController.getInstance().getRightHorizontal());
+			sloth.setRightVert(InputController.getInstance().getRightVertical());
+			sloth.setLeftGrab(InputController.getInstance().getLeftGrab());
+			sloth.setRightGrab(InputController.getInstance().getRightGrab());
+			sloth.setLeftStickPressed(InputController.getInstance().getLeftStickPressed());
+			sloth.setRightStickPressed(InputController.getInstance().getRightStickPressed());
+			currentTime += dt;
 
-		if (sloth.isRightGrab()) {
-			sloth.grab(world,rightCollisionBody, false);
-		} else {
-			sloth.releaseRight(world);
-		}
+			//#TODO Collision states check
+			setFailure(collisions.isFlowKill());
 
-		// Normal physics
-		sloth.doThePhysics();
+			Body leftCollisionBody = collisions.getLeftBody();
+			Body rightCollisionBody = collisions.getRightBody();
 
-		// If we use sound, we must remember this.
-		SoundController.getInstance().update();
-
-		if (isComplete()) {
-			float record = currentTime;
-			if(record < lm.getRecordTime()) {
-				lm.setRecordTime(record);
-				if (jsonLoaderSaver.saveLevel(lm, loadLevel))
-					System.out.println("New record time for this level!");
+			if (collisions.isFlowWin()) {
+				System.out.println("VICTORY");
+				setComplete(true);
 			}
-			int current = GlobalConfiguration.getInstance().getCurrentLevel();
-			GlobalConfiguration.getInstance().setCurrentLevel(current+1);
-			System.out.println("GG");
-			listener.exitScreen(this, EXIT_GM_GM);
-		}
 
-		if(isFailure()){
-			System.out.println("Fail");
-			reset();
+			// Physics tiem
+			// Gribby grab
+			if (sloth.isLeftGrab()) {
+				sloth.grab(world, leftCollisionBody, true);
+			} else {
+				sloth.releaseLeft(world);
+			}
+
+			if (sloth.isRightGrab()) {
+				sloth.grab(world, rightCollisionBody, false);
+			} else {
+				sloth.releaseRight(world);
+			}
+
+			// Normal physics
+			sloth.doThePhysics();
+
+			// If we use sound, we must remember this.
+			SoundController.getInstance().update();
+
+			if (isComplete()) {
+				float record = currentTime;
+				if (record < lm.getRecordTime()) {
+					lm.setRecordTime(record);
+					if (jsonLoaderSaver.saveLevel(lm, loadLevel))
+						System.out.println("New record time for this level!");
+				}
+				int current = GlobalConfiguration.getInstance().getCurrentLevel();
+				GlobalConfiguration.getInstance().setCurrentLevel(current + 1);
+				System.out.println("GG");
+				listener.exitScreen(this, EXIT_GM_GM);
+			}
+
+			if (isFailure()) {
+				System.out.println("Fail");
+				reset();
+			}
 		}
+		prevPaused = paused;
 	}
 
 	public void draw(float delta){
@@ -401,7 +457,7 @@ public class GameModeController extends WorldController {
 			obj.draw(canvas);
 		}
 
-		if (!playerIsReady)
+		if (!playerIsReady && !paused)
 			printHelp();
 		canvas.end();
 		sloth.drawGrab(canvas, camTrans);
@@ -424,6 +480,22 @@ public class GameModeController extends WorldController {
 			sloth.drawForces(canvas, camTrans);
 		}
 
+		// draw pause menu stuff over everything
+		if (paused) {
+			canvas.begin();
+			canvas.draw(pauseTexture);
+
+			canvas.draw(fern, Color.WHITE,fern.getWidth()/2, fern.getHeight()/2,
+					pause_locs[pause_mode].x * worldScale.x, pause_locs[pause_mode].y* worldScale.y,
+					0,worldScale.x/fern.getWidth(),worldScale.y/fern.getHeight());
+
+			canvas.end();
+		}
+
+		canvas.begin();
+		if (prevPaused && !paused && !playerIsReady)
+			printHelp();
+		canvas.end();
 	}
 
 }
