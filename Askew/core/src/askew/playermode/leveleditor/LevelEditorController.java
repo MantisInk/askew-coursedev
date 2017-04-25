a7b98877d480f9e9a855e2e8873faa9981786bde
Henry Chen <hjc77@cornell.edu>
2017-04-25 10:43:44 -0400

    changed scale to .5

--- Askew/core/src/askew/playermode/leveleditor/LevelEditorController.java
+++ Askew/core/src/askew/playermode/leveleditor/LevelEditorController.java
@@ -119,7 +119,7 @@ public class LevelEditorController extends WorldController {
 
 	//In Pixels, divide by world scale for box2d.
 	public static final float GUI_LOWER_BAR_HEIGHT = 200.f;
-	public static final float GUI_LEFT_BAR_WIDTH = 400.f;
+	public static final float GUI_LEFT_BAR_WIDTH = 200.f;
 
 	public float MAX_SNAP_DISTANCE = 1f;
 	public float CAMERA_PAN_SPEED = 20f;
@@ -594,10 +594,10 @@ public class LevelEditorController extends WorldController {
 				// down
 				adjustedCyCamera += CAMERA_PAN_SPEED/worldScale.y;
 			}
-			if (mouseX > (16f ) - 1) {
+			if (mouseX > (bounds.getWidth() ) - 1) {
 				adjustedCxCamera -= CAMERA_PAN_SPEED/worldScale.x;
 			}
-			if (mouseY > (9f ) - 1) {
+			if (mouseY > (bounds.getHeight() ) - 1) {
 				adjustedCyCamera -= CAMERA_PAN_SPEED/worldScale.y;
 			}
 			if(InputController.getInstance().isSpaceKeyPressed()){
@@ -1010,20 +1010,20 @@ public class LevelEditorController extends WorldController {
 		canvas.begin(); // DO NOT SCALE
 		if (showHelp) {
 			String[] splitHelp = HELP_TEXT.split("\\R");
-			float beginY = 7f * worldScale.y;
+			float beginY = GUI_LOWER_BAR_HEIGHT + (9f * worldScale.y);
 			for (int i = 0; i < splitHelp.length; i++) {
 				canvas.drawTextStandard(splitHelp[i], GUI_LEFT_BAR_WIDTH + 10, beginY);
-				beginY -= .2 * worldScale.y;
+				beginY -= .3 * worldScale.y;
 			}
 		}
 
 
-		canvas.drawTextStandard("MOUSE: " + adjustedMouseX + " , " + adjustedMouseY, GUI_LEFT_BAR_WIDTH + 10, 2.8f * worldScale.y);
-		canvas.drawTextStandard(-adjustedCxCamera + "," + -adjustedCyCamera , GUI_LEFT_BAR_WIDTH + 10, 2.6f * worldScale.y);
-		canvas.drawTextStandard("Level: " + currentLevel, GUI_LEFT_BAR_WIDTH + 10, 2.4f * worldScale.y);
-		canvas.drawTextStandard("Creating: " + creationOptions[tentativeEntityIndex], GUI_LEFT_BAR_WIDTH + 10, 2.2f * worldScale.y);
+		canvas.drawTextStandard("MOUSE: " + adjustedMouseX + " , " + adjustedMouseY, GUI_LEFT_BAR_WIDTH + 10, GUI_LOWER_BAR_HEIGHT + (1.4f * worldScale.y));
+		canvas.drawTextStandard(-adjustedCxCamera + "," + -adjustedCyCamera , GUI_LEFT_BAR_WIDTH + 10, GUI_LOWER_BAR_HEIGHT + (1.1f * worldScale.y));
+		canvas.drawTextStandard("Level: " + currentLevel, GUI_LEFT_BAR_WIDTH + 10, GUI_LOWER_BAR_HEIGHT + (.8f * worldScale.y));
+		canvas.drawTextStandard("Creating: " + creationOptions[tentativeEntityIndex], GUI_LEFT_BAR_WIDTH + 10, GUI_LOWER_BAR_HEIGHT + (.5f * worldScale.y));
 		if (tentativeEntityIndex != entityIndex) {
-			canvas.drawTextStandard("Hit Enter to Select New Object Type.", GUI_LEFT_BAR_WIDTH + 10, 2f * worldScale.y);
+			canvas.drawTextStandard("Hit Enter to Select New Object Type.", GUI_LEFT_BAR_WIDTH + 10, GUI_LOWER_BAR_HEIGHT + (.2f * worldScale.y));
 		}
 		canvas.end();
 
