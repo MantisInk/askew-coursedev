22f24d02cfb0fb762f0656056a4ba711a020fdc1
Trevor Edwards <tre23@cornell.edu>
2017-04-28 22:03:22 -0400

    update owl texture

--- Askew/core/src/askew/util/MyPacker.java
+++ Askew/core/src/askew/util/MyPacker.java
@@ -2,11 +2,8 @@ package askew.util;
 
 import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
-import java.util.Scanner;
-
 public class MyPacker {
     public static void main (String[] args) throws Exception {
-        Scanner in = new Scanner(System.in);
         System.out.println("inputDir");
         String inputDir = "texture/animated";
         System.out.println("outputDir");
