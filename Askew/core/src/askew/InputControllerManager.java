7244b2e721f9218e50e475f5fbb2619c278543cd
Trevor Edwards <tre23@cornell.edu>
2017-05-01 14:54:05 -0400

    cleanup
    oops the controllermanager
    
    p for level editor
    
    fix menu up down

---
+++ Askew/core/src/askew/InputControllerManager.java
@@ -0,0 +1,37 @@
+package askew;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class InputControllerManager {
+
+    public static final int NUM_CONTROLLERS = 2;
+
+    private static InputControllerManager instance;
+
+    private List<InputController> inputControllerList;
+
+    public static InputControllerManager getInstance() {
+        if (instance == null) {
+            instance = new InputControllerManager();
+        }
+
+        return instance;
+    }
+
+    private InputControllerManager() {
+        this.inputControllerList = new ArrayList<>();
+        for (int i = 0; i < NUM_CONTROLLERS; ++i) {
+            inputControllerList.add(new InputController(i));
+        }
+    }
+
+    public InputController getController(int index) {
+        return inputControllerList.get(index);
+    }
+
+    public List<InputController> inputControllers() {
+        return inputControllerList;
+    }
+}
