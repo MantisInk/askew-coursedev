3c8d93ad3afdc5ad487b06df6bffb2b86d55daa9
echyam <echyam@gmail.com>
2017-05-02 00:46:45 -0400

    added settings menu

--- Askew/core/src/askew/playermode/WorldController.java
+++ Askew/core/src/askew/playermode/WorldController.java
@@ -328,8 +328,8 @@ public abstract class WorldController implements Screen {
 	 */
 	public void setCanvas(GameCanvas canvas) {
 		this.canvas = canvas;
-		this.worldScale.x = 1.0f * (float)canvas.getWidth()/(float)bounds.getWidth();
-		this.worldScale.y = 1.0f * (float)canvas.getHeight()/(float)bounds.getHeight();
+		this.worldScale.x = 1.3f * (float)canvas.getWidth()/(float)bounds.getWidth();
+		this.worldScale.y = 1.3f * (float)canvas.getHeight()/(float)bounds.getHeight();
 		//System.out.println("SETCANVAS SET SCALE");
 	}
 	
