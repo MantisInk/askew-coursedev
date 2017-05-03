8d07c908fc8d7881cc681ad3fc496008c2f947f4
Trevor Edwards <tre23@cornell.edu>
2017-05-02 21:59:08 -0400

    AMAZING WALL EDITOR

--- Askew/core/src/askew/entity/wall/WallModel.java
+++ Askew/core/src/askew/entity/wall/WallModel.java
@@ -57,5 +57,113 @@ public class WallModel extends PolygonObstacle {
         this.y = y;
     }
 
+    public void pinchCreate(float bdx, float bdy) {
+        // find the closest point in the existing points list and insert after.
+        // the closest point minimizes pinch distance
+        int index = -1;
+        float minDst = Float.MAX_VALUE;
+        for (int i = 0; i < points.length; i+=2){
+            float curX = points[i];
+            float curY = points[i+1];
+            float nextX = points[(i+2)%points.length];
+            float nextY = points[(i+3)%points.length];
+            float dx = curX - bdx;
+            float dy = curY - bdy;
+            float dst = (float) Math.sqrt(dx*dx + dy*dy);
+            float dx2 = nextX - bdx;
+            float dy2 = nextY - bdy;
+            float dst2 = (float) Math.sqrt(dx2*dx2 + dy2*dy2);
+            if ((dst+dst2) < minDst) {
+                index = i;
+                minDst = dst+dst2;
+            }
+        }
+
+        float[] newPoints = new float[points.length+2];
+        for (int i = 0; i <= index; i+=2) {
+            newPoints[i] = points[i];
+            newPoints[i+1] = points[i+1];
+        }
+
+        newPoints[index+2] = bdx;
+        newPoints[index+3] = bdy;
+
+        for (int i = index+4; i < newPoints.length; i+=2) {
+            newPoints[i] = points[i-2];
+            newPoints[i+1] = points[i+1-2];
+        }
+        this.points = newPoints;
+        this.initShapes(points);
+        this.initBounds();
+    }
+
+    public void pinchDelete(float bdx, float bdy) {
+        // find the closest point in the existing points list and insert after.
+        // the closest point minimizes pinch distance
+        int index = -1;
+        float minDst = Float.MAX_VALUE;
+        //DONTFIXME: start at index 2 because origin must be 0,0
+        for (int i = 2; i < points.length; i+=2){
+            float curX = points[i];
+            float curY = points[i+1];
+            float dx = curX - bdx;
+            float dy = curY - bdy;
+            float dst = (float) Math.sqrt(dx*dx + dy*dy);
+            if ((dst) < minDst) {
+                index = i;
+                minDst = dst;
+            }
+        }
+
+
+        float[] newPoints = new float[points.length-2];
+        for (int i = 0; i < index; i+=2) {
+            newPoints[i] = points[i];
+            newPoints[i+1] = points[i+1];
+        }
+
+        // goodbye old pointo
+
+        for (int i = index; i < newPoints.length; i+=2) {
+            newPoints[i] = points[i+2];
+            newPoints[i+1] = points[i+1+2];
+        }
+        this.points = newPoints;
+        this.initShapes(points);
+        this.initBounds();
+    }
+
+    public void pinchMove(float bdx, float bdy) {
+        // find the closest point in the existing points list and insert after.
+        // the closest point minimizes pinch distance
+        int index = -1;
+        float minDst = Float.MAX_VALUE;
+        //DONTFIXME: start at index 2 because origin must be 0,0
+        for (int i = 2; i < points.length; i+=2){
+            float curX = points[i];
+            float curY = points[i+1];
+            float dx = curX - bdx;
+            float dy = curY - bdy;
+            float dst = (float) Math.sqrt(dx*dx + dy*dy);
+            if ((dst) < minDst) {
+                index = i;
+                minDst = dst;
+            }
+        }
+
+        points[index] = bdx;
+        points[index+1] = bdy;
+
+        this.initShapes(points);
+        this.initBounds();
+    }
+
+    public float getModelX() {
+        return this.x;
+    }
+
+    public float getModelY() {
+        return this.y;
+    }
 }
 
