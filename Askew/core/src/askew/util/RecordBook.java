de363f908e946f1a71b3262d9e5d0b9178529666
echyam <echyam@gmail.com>
2017-05-02 23:44:12 -0400

    save records in their own json

---
+++ Askew/core/src/askew/util/RecordBook.java
@@ -0,0 +1,54 @@
+package askew.util;
+
+import askew.util.json.JSONLoaderSaver;
+import com.google.gson.JsonObject;
+
+/**
+ * A model class containing variables which we set as configurable for faster prototyping and modding.
+ * Uses the singleton pattern.
+ */
+public class RecordBook {
+
+    private static final String CONFIG_PATH = "levels/records.json";
+    private static RecordBook instance;
+
+    private JsonObject dataBlob;
+
+    public static RecordBook getInstance() {
+        if (instance == null) {
+            update();
+        }
+        return instance;
+    }
+
+    /* Creates a new instance populated with the current values of the records.json. */
+    public static void update() {
+        JsonObject newBlob = JSONLoaderSaver.loadArbitrary(CONFIG_PATH).orElseThrow(RuntimeException::new);
+        instance = new RecordBook();
+        instance.dataBlob = newBlob;
+    }
+
+    private RecordBook() {
+        dataBlob = new JsonObject();
+    }
+
+    public float getRecord(String lvlname) {
+        try {
+            return dataBlob.get(lvlname).getAsFloat();
+        } catch (NullPointerException e) {
+            System.err.print("Level record doesn't exist!");
+            return 0f;
+        }
+    }
+
+    public boolean setRecord(String lvlname, float record) {
+        try {
+            dataBlob.remove(lvlname);
+            dataBlob.addProperty(lvlname, record);
+        } catch (NullPointerException e) {
+            System.err.print("Level record doesn't exist!");
+            return false;
+        }
+        return true;
+    }
+}
