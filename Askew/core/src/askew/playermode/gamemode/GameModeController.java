9e9be9a8bb327f6069ecd233599d709ad0287ecf
Trevor Edwards <tre23@cornell.edu>
2017-05-05 02:30:02 -0400

    fix taser

--- Askew/core/src/askew/playermode/gamemode/GameModeController.java
+++ Askew/core/src/askew/playermode/gamemode/GameModeController.java
@@ -584,16 +584,20 @@ public class GameModeController extends WorldController {
 			Body leftCollisionBody = collisions.getLeftBody();
 			Body rightCollisionBody = collisions.getRightBody();
 
-			sloth.setLeftHori(InputControllerManager.getInstance().getController(0).getLeftHorizontal());
-			sloth.setLeftVert(InputControllerManager.getInstance().getController(0).getLeftVertical());
-			sloth.setRightHori(InputControllerManager.getInstance().getController(0).getRightHorizontal());
-			sloth.setRightVert(InputControllerManager.getInstance().getController(0).getRightVertical());
-			sloth.setLeftGrab(InputControllerManager.getInstance().getController(0).getLeftGrab());
-			sloth.setRightGrab(InputControllerManager.getInstance().getController(0).getRightGrab());
-			sloth.setSafeGrab(InputControllerManager.getInstance().getController(0).isBottomButtonPressed(), leftCollisionBody, rightCollisionBody, world);
-			sloth.setOneGrab(InputControllerManager.getInstance().getController(0).getRightGrab());
-			sloth.setLeftStickPressed(InputControllerManager.getInstance().getController(0).getLeftStickPressed());
-			sloth.setRightStickPressed(InputControllerManager.getInstance().getController(0).getRightStickPressed());
+			// Prevent control input if flow is win
+			if (!collisions.isFlowWin()) {
+				sloth.setLeftHori(InputControllerManager.getInstance().getController(0).getLeftHorizontal());
+				sloth.setLeftVert(InputControllerManager.getInstance().getController(0).getLeftVertical());
+				sloth.setRightHori(InputControllerManager.getInstance().getController(0).getRightHorizontal());
+				sloth.setRightVert(InputControllerManager.getInstance().getController(0).getRightVertical());
+				sloth.setLeftGrab(InputControllerManager.getInstance().getController(0).getLeftGrab());
+				sloth.setRightGrab(InputControllerManager.getInstance().getController(0).getRightGrab());
+				sloth.setSafeGrab(InputControllerManager.getInstance().getController(0).isBottomButtonPressed(), leftCollisionBody, rightCollisionBody, world);
+				sloth.setOneGrab(InputControllerManager.getInstance().getController(0).getRightGrab());
+				sloth.setLeftStickPressed(InputControllerManager.getInstance().getController(0).getLeftStickPressed());
+				sloth.setRightStickPressed(InputControllerManager.getInstance().getController(0).getRightStickPressed());
+			}
+
 			currentTime += dt;
 
 			//#TODO Collision states check
@@ -613,7 +617,9 @@ public class GameModeController extends WorldController {
 						sloth.grab(world, owl.getBody(), false);
 					}
 				}
+
 				coverOpacity = owl.doVictory();
+
 				if (owl.didVictory()) {
 					setComplete(true);
 				}
@@ -633,11 +639,11 @@ public class GameModeController extends WorldController {
 				}
 			}
 
-			if (sloth.isGrabbedEntity()) {
+			if (sloth.isGrabbedEntity() && !collisions.isFlowWin()) {
 				grabSound.play();
 			}
 
-			if (sloth.isReleasedEntity()) {
+			if (sloth.isReleasedEntity() && !collisions.isFlowWin()) {
 				releaseSound.play();
 			}
 
