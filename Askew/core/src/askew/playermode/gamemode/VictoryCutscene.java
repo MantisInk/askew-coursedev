a801afee1a8a53792479abafab46e88707df874a
Trevor Edwards <tre23@cornell.edu>
2017-05-18 23:38:16 -0400

    animation checkpoint

--- Askew/core/src/askew/playermode/gamemode/VictoryCutscene.java
+++ Askew/core/src/askew/playermode/gamemode/VictoryCutscene.java
@@ -9,21 +9,32 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class VictoryCutscene {
 
-    Animation cutsceneAnimation;
+    Animation happyFlowAnimation;
+    Animation sadFlowAnimation;
+    Animation happyEbbAnimation;
 
     private float elapseTime;
     private boolean didFirstFrame;
+    private int frameNumber;
+    private int previousFrame;
+
+    private static final int CUTOFF_A = 32;
+    private static final int CUTOFF_B = 64;
 
     public VictoryCutscene(MantisAssetManager manager) {
-        cutsceneAnimation = new Animation(0.090f, manager.getTextureAtlas()
-                .findRegions("cutscene"), Animation.PlayMode.NORMAL);
-        if (cutsceneAnimation.getKeyFrames().length == 0) System.err.println
-                ("did not find cutscene");
+        happyFlowAnimation = new Animation(0.070f, manager.getTextureAtlas()
+                .findRegions("happyswing"), Animation.PlayMode.NORMAL);
+        happyFlowAnimation = new Animation(0.070f, manager.getTextureAtlas()
+                .findRegions("sadswing"), Animation.PlayMode.NORMAL);
+        happyEbbAnimation = new Animation(0.060f, manager.getTextureAtlas()
+                .findRegions("ebbswing"), Animation.PlayMode.NORMAL);
         reset();
     }
 
     public void reset() {
         elapseTime = 0;
+        frameNumber = 0;
+        previousFrame = 0;
         didFirstFrame = false;
     }
 
@@ -36,12 +47,30 @@ public class VictoryCutscene {
             didFirstFrame = true;
         }
 
-        TextureRegion drawFrame = cutsceneAnimation.getKeyFrame(elapseTime,
-                false);
-        canvas.draw(drawFrame, Color.WHITE,canvas.getWidth()/2,(int)(1.0*canvas
-                        .getHeight()/4.0),canvas
-                        .getWidth()/2,
-                canvas
-                .getHeight()/2);
+        float cw = canvas.getWidth();
+        float ch = canvas.getHeight();
+
+        TextureRegion drawFrame;
+        if (frameNumber < CUTOFF_A) {
+            drawFrame = happyFlowAnimation.getKeyFrame(elapseTime, true);
+            canvas.draw(drawFrame, Color.WHITE,(int)(cw/2) + (cw/6f), (int)(ch/4)
+                    ,(int)
+                            (cw/4f), (int)(ch/4f));
+
+            drawFrame = happyEbbAnimation.getKeyFrame(elapseTime,
+                    true);
+            canvas.draw(drawFrame, Color.WHITE,(int)(cw/2),
+                    (int)
+                            (ch/4) + ((cw/4f)-(cw/6f))/2f,(int)
+                            (cw/6f), (int)(ch/6f));
+            int ebbFrame = happyEbbAnimation.getKeyFrameIndex(elapseTime);
+            if (ebbFrame != previousFrame) frameNumber++;
+            previousFrame = ebbFrame;
+        } else if (frameNumber < CUTOFF_B) {
+
+        } else {
+
+        }
+
     }
 }
