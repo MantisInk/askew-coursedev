783c26063318b1b6a49ecc3c1f1ec42508aa8a82
Henry Chen <emeraldarrow@gmail.com>
2017-05-19 13:44:59 -0400

    added particles back

--- Askew/core/src/askew/playermode/gamemode/GameModeController.java
+++ Askew/core/src/askew/playermode/gamemode/GameModeController.java
@@ -161,8 +161,8 @@ public class GameModeController extends WorldController {
 	protected float coverOpacity;
 
 	protected ParticleController particleController;
-	protected static final int MAX_PARTICLES = 0;
-	protected static final int INITIAL_FOG = 0;
+	protected static final int MAX_PARTICLES = 2000;
+	protected static final int INITIAL_FOG = 50;
 
 	protected float fogTime,eyeTime;
 	private int levelCompleteJunkState;
